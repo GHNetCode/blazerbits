04841e00be7c9beb375aa8bb5a854f13e02a22b6
GHNetCode <GHnetcode2020@yahoo.com>
2026-04-30 21:33:20 +0100

    added Gtranslate plugin..

--- DEV/script.js
+++ DEV/script.js
@@ -2,6 +2,32 @@ const slider = document.getElementById("themeSlider");
 const root = document.documentElement;
 const menu = document.getElementById("menu");
 
+
+
+
+window.gtranslateSettings = {
+  default_language: "en",
+  native_language_names: true,
+  detect_browser_language: true,
+  languages: ["en","nl","fr","de","hu","it","es"],
+  wrapper_selector: ".gtranslate_wrapper",
+  flag_size: 16,
+  // switcher_vertical_position: "top",
+  switcher_horizontal_position:"inline",
+  flag_style: "3d",
+  switcher_text_color:"#f7f7f7",
+  switcher_arrow_color:"#f2f2f2",
+  switcher_border_color:"#161616",
+  switcher_border_radius:"10",
+  switcher_background_color:"#303030",
+  switcher_background_shadow_color:"#474747",
+  switcher_background_hover_color:"#3a3a3a",
+  dropdown_text_color:"#eaeaea",
+  dropdown_hover_color:"#748393",
+  dropdown_background_color:"#474747"
+};
+
+
 /*Temporarily disabled due to navigation bar added*/
 // const hamburger = document.getElementById("hamburger");
 
@@ -54,3 +80,4 @@ function interpolateColor(start, end, factor) {
       hamburger.classList.remove("active");
     }
   });
+
