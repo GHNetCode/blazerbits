5c9d345d92cc4a21ec132de4da4669a27ce8b82d
GHNetCode <GHnetcode2020@yahoo.com>
2026-05-03 15:44:48 +0100

    ..

--- DEV/script.js
+++ DEV/script.js
@@ -5,27 +5,6 @@ const menu = document.getElementById("menu");
 
 
 
-window.gtranslateSettings = {
-  default_language: "en",
-  native_language_names: true,
-  detect_browser_language: true,
-  languages: ["en","hu","cy","ga","nl","fr","de","it","es"],
-  wrapper_selector: ".gtranslate_wrapper",
-  flag_size: 16,
-  // switcher_vertical_position: "top",
-  switcher_horizontal_position:"inline",
-  flag_style: "3d",
-  switcher_text_color:"#f7f7f7",
-  switcher_arrow_color:"#f2f2f2",
-  switcher_border_color:"#161616",
-  switcher_border_radius:"10",
-  switcher_background_color:"#040308",
-  switcher_background_shadow_color:"#232323",
-  switcher_background_hover_color:"#3a3a3a",
-  dropdown_text_color:"#eaeaea",
-  dropdown_hover_color:"#3a3a3a",
-  dropdown_background_color:"#040308"
-};
 
 
 /*Temporarily disabled due to navigation bar added*/
@@ -66,6 +45,29 @@ function interpolateColor(start, end, factor) {
   return start.map((s, i) => Math.round(s + factor * (end[i] - s))).join(",");
 }
 
+
+
+window.gtranslateSettings = {
+  default_language: "en",
+  native_language_names: true,
+  detect_browser_language: true,
+  languages: ["en","hu","cy","ga","nl","fr","de","it","es"],
+  wrapper_selector: ".gtranslate_wrapper",
+  flag_size: 16,
+  // switcher_vertical_position: "top",
+  switcher_horizontal_position:"inline",
+  flag_style: "3d",
+  switcher_text_color:"#f7f7f7",
+  switcher_arrow_color:"#f2f2f2",
+  switcher_border_color:"#161616",
+  switcher_border_radius:"10",
+  switcher_background_color:"#040308",
+  switcher_background_shadow_color:"#232323",
+  switcher_background_hover_color:"#3a3a3a",
+  dropdown_text_color:"#eaeaea",
+  dropdown_hover_color:"#3a3a3a",
+  dropdown_background_color:"#040308"
+};
 
 
 /* MENU TOGGLE */ //Temporarily disabled due to navigation bar added.
